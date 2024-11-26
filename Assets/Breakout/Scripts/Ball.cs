b64560dff67706450efd3f7277064a5ab379a492
Utz Stauder <utzstauder@users.noreply.github.com>
2024-11-26 15:26:42 +0100

    added speed controls to ball logic

--- Assets/Breakout/Scripts/Ball.cs
+++ Assets/Breakout/Scripts/Ball.cs
@@ -8,6 +8,11 @@ public class Ball : MonoBehaviour
 
     public float initialSpeed = 10f;
 
+    [Range(0.95f, 1.05f)]
+    public float speedIncreaseMultiplier = 1f;
+
+    private float currentSpeed;
+
     private void Awake()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
@@ -15,6 +20,14 @@ public class Ball : MonoBehaviour
 
     private void Start()
     {
-        rigidbody2d.velocity = new Vector2(0, initialSpeed);
+        currentSpeed = initialSpeed;
+        rigidbody2d.velocity = new Vector2(0, initialSpeed); ;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentSpeed *= speedIncreaseMultiplier;
+
+        rigidbody2d.velocity = rigidbody2d.velocity.normalized * currentSpeed;
     }
 }
